c7347c2c37e8161353f32086c163096190481034
James Teasdale <teasdale.james@gmail.com>
2022-11-19 14:07:28 -0500

    woo

---
+++ client_options.py
@@ -0,0 +1,14 @@
+def display_options():
+        print("Python DB Menu\n")
+        print("1. Find Customer")
+        print("2. Add Customer")
+        print("3. Delete Customer")
+        print("4. Update Customer Age")
+        print("5 Update Customer address")
+        print("6. Update customer phone")
+        print("7. Print Report")
+        print("8. Exit\n")
+        print("Select: ")
+
+
+
